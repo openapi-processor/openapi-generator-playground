71534a7edb01599a6af91613b3da354b4baa541b
Martin Hauner <martin.hauner@gmx.net>
2020-10-04 15:13:38 +0200

    simple openapi with & generator/processor setup

---
+++ src/main/groovy/io/openapiprocessor/generator/GeneratorApplication.groovy
@@ -0,0 +1,13 @@
+package io.openapiprocessor.generator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GeneratorApplication {
+
+    static void main (String[] args) {
+        SpringApplication.run (GeneratorApplication, args)
+    }
+
+}
